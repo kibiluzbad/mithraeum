9d72857db5e1023d4ae03ff0dc83ebb87959dc06
Leonardo Cardoso <kibiluzbad@gmail.com>
2012-09-12 17:32:18 -0300

    Adding Thoth movies finder.

--- src/Api/Mithraeum.Api/Modules/IMoviesFinder.cs
+++ src/Api/Mithraeum.Api/Modules/IMoviesFinder.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 using Mithraeum.Api.Model;
+using Nancy.Json;
 
 namespace Mithraeum.Api.Modules
 {
@@ -7,5 +12,55 @@ namespace Mithraeum.Api.Modules
     {
         IEnumerable<FinderOption> FindByName(string name);
         Movie FindByImdbId(FinderOption imdbid);
+    }
+
+    public class Thoth : IMoviesFinder
+    {
+        private const string Root = "http://thothapp.heroku.com/";
+        
+        public IEnumerable<FinderOption> FindByName(string name)
+        {
+            string json = HttpHelper.Get(string.Format("{0}/search/{1}", Root, name));
+            var serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<IEnumerable<FinderOption>>(json);
+        }
+
+        public Movie FindByImdbId(FinderOption imdbid)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public static class HttpHelper
+    {
+        public static string Get(string url, IDictionary<string,object> parameters = null)
+        {
+            var sb = new StringBuilder();
+            var buf = new byte[8192];
+
+            var request = (HttpWebRequest)
+                WebRequest.Create(url);
+
+            var response = (HttpWebResponse)
+                request.GetResponse();
+
+            Stream resStream = response.GetResponseStream();
+
+            int count = 0;
+
+            do
+            {
+                if (resStream != null) count = resStream.Read(buf, 0, buf.Length);
+
+                if (count == 0) continue;
+
+                string tempString = Encoding.ASCII.GetString(buf, 0, count);
+                
+                sb.Append(tempString);
+            }
+            while (count > 0);
+
+            return sb.ToString();
+        }
     }
 }
--- src/Api/Mithraeum.Api/Modules/Movies.cs
+++ src/Api/Mithraeum.Api/Modules/Movies.cs
@@ -10,14 +10,22 @@ namespace Mithraeum.Api.Modules
     public class Movies
         : NancyModule
     {
-        public Movies(IDocumentSession session, IMoviesFinder finder)
+        private readonly IDocumentSession _session;
+        private readonly IMoviesFinder _finder;
+
+        public Movies(IDocumentSession session,
+                      IMoviesFinder finder)
             : base("/api/movies")
         {
-            After += _ => session.SaveChanges();
+            _session = session;
+            _finder = finder;
+
+
+            After += _ => _session.SaveChanges();
 
             Get["/"] = _ =>
                            {
-                               var movies = session.Load<Movie>();
+                               var movies = _session.Load<Movie>();
 
                                return Response
                                    .AsJson(movies);
@@ -25,14 +33,16 @@ namespace Mithraeum.Api.Modules
 
             Post["/"] = _ =>
                             {
-                                
                                 var title = (string) Request.Form.name;
-                                IEnumerable<FinderOption> list = finder.FindByName(title);
+
+                                IEnumerable<FinderOption> list = _finder.FindByName(title);
 
                                 if (1 == list.Count())
                                 {
-                                    Movie movie = finder.FindByImdbId(list.FirstOrDefault());
-                                    session.Store(movie,movie.Imdbid);
+                                    Movie movie = _finder.FindByImdbId(list.FirstOrDefault());
+
+                                    _session.Store(movie, movie.Imdbid);
+
                                     return Response.AsJson(movie);
                                 }
 
@@ -42,7 +52,7 @@ namespace Mithraeum.Api.Modules
                                                              Options = list
                                                          };
 
-                                session.Store(ambigousResult, ambigousResult.Title);
+                                _session.Store(ambigousResult, ambigousResult.Title);
 
                                 return Response
                                     .AsJson(ambigousResult);
